cb682799e9928e652ad62c7393ebc5f8b3615032
Tyler <TZM2002@protonmail.com>
2025-05-11 18:39:16 +0200

    added_map

--- app/berlin/page.tsx
+++ app/berlin/page.tsx
@@ -1,29 +1,29 @@
 "use client";
-
 import React from 'react';
-import { APIProvider, Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 
 const berlinCenter = { lat: 52.52, lng: 13.405 };
-const mapContainerStyle = { width: '100%', height: '400px' };
+const mapContainerStyle = { width: '100%', height: '500px' };
 const GOOGLE_MAPS_API_KEY = 'AIzaSyCjuYqAbBrtDyQ7x7Idey8S1-hJQb4_V-A';
 
-const BerlinPage: React.FC = () => {
+// 🔥 Paste your MapId here
+const MAP_ID = '1228b90159890bac98703c1c';
+
+const MapsWithLocations: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-white font-geistSans">
-      <div className="text-center">
-        <h1 className="berlin text-4xl font-bold text-black mb-6">
-          My Livelihood
-        </h1>
+      <div className="text-center w-full">
+        <h1 className="text-4xl font-bold text-black mb-6">Our Locations</h1>
         <div style={mapContainerStyle}>
-          <APIProvider apiKey={GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+          <APIProvider apiKey={GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>            
             <Map
-              defaultZoom={12}
+              defaultZoom={10}
               defaultCenter={berlinCenter}
-              onCameraChanged={(ev: MapCameraChangedEvent) =>
-                console.log('Camera changed:', ev.detail.center, 'Zoom:', ev.detail.zoom)
-              }
+              mapId={MAP_ID}
               style={{ width: '100%', height: '100%' }}
-            />
+            >
+              {/* Add your marker data here */}
+            </Map>
           </APIProvider>
         </div>
       </div>
@@ -31,4 +31,4 @@ const BerlinPage: React.FC = () => {
   );
 };
 
-export default BerlinPage;
+export default MapsWithLocations;
