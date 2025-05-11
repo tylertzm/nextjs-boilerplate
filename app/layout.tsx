19e6631275956593b2dcc29b7b44bc92e0c7f21a
Tyler <TZM2002@protonmail.com>
2025-05-11 23:15:00 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,7 @@ export default function RootLayout({
         </Link>
       </nav>
         {children}
+        <Analytics />
       </body>
     </html>
   );
